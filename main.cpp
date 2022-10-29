dd81a76420f08606a9be6dafc847ea8bdc0e8084
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-10-29 19:14:36 +0700

    Implement test program

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "classes.hpp"
+
+int main()
+{
+    list *h = new list(4);
+    h->add(8);
+    h->add(9);
+    h->lookup(4);
+    h->lookup(5);
+    h->lookup(8);
+    h->lookup(9);
+
+    int arr[3] = {2, 4, 1};
+    stack s(arr, 3);
+    std::cout << s.pop() << std::endl;
+    std::cout << s.pop() << std::endl;
+    std::cout << s.pop() << std::endl;
+    std::cout << s.pop() << std::endl;
+
+    queue q(arr, 3);
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
+    std::cout << q.dequeue() << std::endl;
+
+    exit(0);
+}
